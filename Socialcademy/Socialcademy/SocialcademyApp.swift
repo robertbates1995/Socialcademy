ffdd4705726c0a68b8c3f92944641234cbe0aaa9
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-11-01 09:07:47 -0700

    First Commit

---
+++ Socialcademy/Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Robert Bates on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
