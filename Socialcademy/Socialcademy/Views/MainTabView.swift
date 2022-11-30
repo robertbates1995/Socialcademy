392284e85ee577743e2a24a85249a72844970f5b
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-11-30 14:28:31 -0800

    added mainTabView

---
+++ Socialcademy/Socialcademy/Views/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  Socialcademy
+//
+//  Created by Robert Bates on 11/30/22.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
