aef1943cabc9f5772ed99939ea10e96cab32afc9
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-12-01 13:54:59 -0800

    created AuthService class

--- Socialcademy/Socialcademy/Services/AuthService.swift
+++ Socialcademy/Socialcademy/Services/AuthService.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+@MainActor
+class AuthService: ObservableObject {
+    @Published var isAuthenticated = false
+    
+    private let auth = Auth.auth()
+    private var listener: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        listener = auth.addStateDidChangeListener { [weak self] _, user in
+            self?.isAuthenticated = user != nil
+        }
+    }
+}
