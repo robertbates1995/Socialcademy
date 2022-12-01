66e560a6172a741d97c0dd5af5563e64aa3c1869
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-11-30 16:29:56 -0800

    implemented mainTabView

--- Socialcademy/Socialcademy/Views/MainTabView.swift
+++ Socialcademy/Socialcademy/Views/MainTabView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            PostsList()
+                .tabItem {
+                    Label("Posts", systemImage: "list.dash")
+                }
+            PostsList(viewModel: PostsViewModel(filter: .favorites))
+                .tabItem {
+                    Label("Favorites", systemImage: "heart")
+                }
+        }
     }
 }
 
