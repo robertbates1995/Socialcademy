7840ee41ec287ecf370c35b8eadf966e10416a10
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-12-02 16:12:23 -0800

    added signIn to AuthViewModel

--- Socialcademy/Socialcademy/ViewModels/AuthViewModel.swift
+++ Socialcademy/Socialcademy/ViewModels/AuthViewModel.swift
@@ -11,9 +11,23 @@ import Foundation
 class AuthViewModel: ObservableObject {
     @Published var isAuthenticated = false
     
+    @Published var email = ""
+    
+    @Published var password = ""
+
     private let authService = AuthService()
     
     init() {
         authService.$isAuthenticated.assign(to: &$isAuthenticated)
+    }
+    
+    func signIn() {
+        Task {
+            do {
+                try await authService.signIn(email: email, password: password)
+            } catch {
+                print("[AuthViewModel] Cannot sign in: \(error)")
+            }
+        }
     }
 }
