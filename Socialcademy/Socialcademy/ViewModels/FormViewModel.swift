5f43c01d720810f755e6f9a09628bd0962bd3157
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2022-12-05 16:22:56 -0800

    added FormViewModel

---
+++ Socialcademy/Socialcademy/ViewModels/FormViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  FormViewModel.swift
+//  Socialcademy
+//
+//  Created by Robert Bates on 12/5/22.
+//
+
+import Foundation
+
+@MainActor
+@dynamicMemberLookup
+class FormViewModel<Value>: ObservableObject {
+    typealias Action = (Value) async throws -> Void
+ 
+    @Published var value: Value
+    @Published var error: Error?
+ 
+    subscript<T>(dynamicMember keyPath: WritableKeyPath<Value, T>) -> T {
+        get { value[keyPath: keyPath] }
+        set { value[keyPath: keyPath] = newValue }
+    }
+ 
+    private let action: Action
+ 
+    init(initialValue: Value, action: @escaping Action) {
+        self.value = initialValue
+        self.action = action
+    }
+ 
+    func submit() {
+        Task {
+            do {
+                try await action(value)
+            } catch {
+                print("[FormViewModel] Cannot submit: \(error)")
+                self.error = error
+            }
+        }
+    }
+}
